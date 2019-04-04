4050f4683487ad1a512bc1d2caf69b080622b050
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-04 20:19:25 -0300

    Add redux and react-redux packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+const store = createStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
