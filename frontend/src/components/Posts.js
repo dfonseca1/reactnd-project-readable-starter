3d52b85f24e2e275e8002e6c3633391795275b16
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-04 20:20:52 -0300

    Add link to post

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Post from "./Post";
+import { Link, withRouter } from 'react-router-dom'
 // import { Container } from './styles';
 
 class Posts extends Component {
@@ -15,7 +16,9 @@ class Posts extends Component {
         ))} */}
         {Object.keys(this.props.posts).map(id => (
           <li key={id}>
-            <Post id={id} />
+           <Link to={`/post/${this.props.posts[id].id}`} className="tweet">
+            <Post post={this.props.posts[id]} />
+            </Link>
           </li>
         ))}
       </div>
