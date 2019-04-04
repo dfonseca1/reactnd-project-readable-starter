08eaba281ccfa5dbeeff306ee5e5a5846ce8e544
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-04 20:22:22 -0300

    Add html structure for Post component

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,13 +3,27 @@ import React, { Component } from "react";
 // import { Container } from './styles';
 
 class Post extends Component {
-    
   render() {
+    let { post } = this.props;
     return (
-        <div>
-            Post ID: {this.props.id}
+      <article>
+        <div className="post">
+          <div className="votes">
+            <span>/\</span>|{post.voteScore}|<span>\/</span>
+          </div>
+          <div className="post-info">
+            <h3>Title: {post.title}</h3>
+            <span> {post.category}</span>
+            <p>
+              Submitted {post.timestamp} ago by {post.author}
+            </p>
+            <p>
+              <span>{post.commentCount} comments</span>
+            </p>
+          </div>
         </div>
-        )
+      </article>
+    );
   }
 }
 
