549b2d9910ca9b71c8afdac3f2f53244c300a608
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-07 18:34:18 -0300

    Implements categoryPage component

--- frontend/src/components/CategoryPage.js
+++ frontend/src/components/CategoryPage.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Posts from "./Posts";
+import { getPostsByCategory } from "../actions/posts";
 
 class CategoryPage extends Component {
-    render(){
-        return(
-            <div>
-                <h3>Posts by Category</h3>
-            </div>
-        )
-    }
+  componentDidMount() {
+    let { name } = this.props.match.params;
+    this.props.dispatch(getPostsByCategory(name));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts by Category</h3>
+        <div>{this.props.posts && <Posts posts={this.props.posts} />}</div>
+      </div>
+    );
+  }
 }
 
-export default CategoryPage
+function mapStateToProps({ posts }) {
+  return posts;
+}
+
+export default connect(mapStateToProps)(CategoryPage);
