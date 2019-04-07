6de02713c17fe3d0ab33baeee7e5001df613e8de
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-07 16:24:33 -0300

    Add conditional render

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -6,31 +6,6 @@ import { connect } from "react-redux";
 
 // import { Container } from './styles';
 
-const posts = {
-  "8xf0y6ziyjabvozdd253nd": {
-    id: "8xf0y6ziyjabvozdd253nd",
-    timestamp: 1467166872634,
-    title: "Udacity is the best place to learn React",
-    body: "Everyone says so after all.",
-    author: "thingtwo",
-    category: "react",
-    voteScore: 6,
-    deleted: false,
-    commentCount: 2
-  },
-  "6ni6ok3ym7mf1p33lnez": {
-    id: "6ni6ok3ym7mf1p33lnez",
-    timestamp: 1468479767190,
-    title: "Learn Redux in 10 minutes!",
-    body: "Just kidding. It takes more than 10 minutes to learn technology.",
-    author: "thingone",
-    category: "redux",
-    voteScore: -5,
-    deleted: false,
-    commentCount: 0
-  }
-};
-
 const categories = [
   {
     name: "react",
@@ -48,8 +23,7 @@ const categories = [
 
 class Dashboard extends Component {
   render() {
-    let postsFromApi = this.props.posts;
-    console.log("Post From APi", postsFromApi);
+    console.log("Dashboard.Props", this.props);
 
     return (
       <div>
@@ -61,15 +35,13 @@ class Dashboard extends Component {
           <Link to="/new">New Post</Link>
         </div>
 
-        <div>
-          <Posts posts={this.props.posts} />
-        </div>
+        <div>{this.props.posts && <Posts posts={this.props.posts} />}</div>
       </div>
     );
   }
 }
 
-function mapStateToProps({posts}){
+function mapStateToProps({ posts }) {
   return posts;
 }
 
