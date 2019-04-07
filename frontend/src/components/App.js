a81a3e1077481b54703adedfd5978bf068afd5db
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-04-07 15:04:41 -0300

    Add loading bar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "../App.css";
 import Dashboard from "./Dashboard";
 import { BrowserRouter, Route } from "react-router-dom";
 import CategoryPage from "./CategoryPage";
 import NewPost from "./NewPost";
 import PostDetailPage from "./PostDetailPage";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
+    console.log("Props.Loading", this.props.loading);
     return (
       <div className="App">
         <BrowserRouter>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/category/:name" component={CategoryPage} />
-          <Route path="/new" component={NewPost} />
-          <Route path="/post/:id" component={PostDetailPage} />
+          <Fragment>
+            <LoadingBar />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/category/:name" component={CategoryPage} />
+                <Route path="/new" component={NewPost} />
+                <Route path="/post/:id" component={PostDetailPage} />
+              </div>
+            )}
+          </Fragment>
         </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  };
+}
+
+export default connect(mapStateToProps)(App);
